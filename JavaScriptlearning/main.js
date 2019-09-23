2a44921533b9a8523a5e9e3d2647de7f47ac2619
Pricillawilson <pricillatrichet@gmail.com>
2019-09-23 19:45:01 +0530

    js

---
+++ JavaScriptlearning/main.js
@@ -0,0 +1,7 @@
+console.log("loaded");
+document.writeln("loaded")
+paper.install(window);
+paper.setup(document.getElementById('mainCanvas'));
+var c= Shape.Circle(200,200,50);
+c.fillColor='green';
+paper.view.draw();
