40ced166857e5fbeabc593b76cbadeec892058c8
Pricillawilson <pricillatrichet@gmail.com>
2019-09-29 19:44:37 +0530

    home component

--- fashionParadise/src/app/app-routing.module.ts
+++ fashionParadise/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HotsalesComponent } from './hotsales/hotsales.component';
 import {ListComponent} from './list/list.component';
 import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {path: 'hotsales', component: HotsalesComponent},{path: 'list', component: ListComponent },
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent},{path: '', component: HomeComponent},
 ];
 
 @NgModule({
--- fashionParadise/src/app/app.module.ts
+++ fashionParadise/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HotsalesComponent } from './hotsales/hotsales.component';
 import { ListComponent } from './list/list.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HotsalesComponent,
     ListComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
