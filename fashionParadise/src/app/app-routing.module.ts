3a6d26651b33240a57760167a509a11d348d9559
Pricillawilson <pricillatrichet@gmail.com>
2019-09-29 11:18:35 +0530

    carousel

--- fashionParadise/src/app/app-routing.module.ts
+++ fashionParadise/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HotsalesComponent } from './hotsales/hotsales.component';
+import {ListComponent} from './list/list.component';
+import {LoginComponent} from './login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hotsales', component: HotsalesComponent},{path: 'list', component: ListComponent },
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fashionParadise/src/app/app.module.ts
+++ fashionParadise/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotsalesComponent } from './hotsales/hotsales.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotsalesComponent,
+    ListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
